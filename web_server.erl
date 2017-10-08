f737586a54a9898bb4713e6e5919f78871d0f5bd
Sergey Yarkin <sega.yarkin@gmail.com>
2017-10-08 23:33:17 +0300

    stage3

--- web_server.erl
+++ web_server.erl
@@ -1,16 +1,31 @@
+-module(web_server).
+-export([start/0, start/1]).
 
-HandleRequest = fun(Sock) ->
+%%
+%% Interface
+%%
+start() -> start(8080).
+start(Port) ->
+    do_start(Port).
+
+%%
+%% Internal
+%%
+do_start(Port) ->
+    Opts = [ {packet, http},
+             {active, false},
+             {backlog, 1024} ],
+    {ok, LSock} = gen_tcp:listen(Port, Opts),
+    accept_loop(LSock).
+
+
+accept_loop(LSock) ->
+    {ok, Sock} = gen_tcp:accept(LSock),
+    handle_request(Sock),
+    accept_loop(LSock).
+
+
+handle_request(Sock) ->
     gen_tcp:recv(Sock, 0),
     gen_tcp:send(Sock, ["HTTP/1.0 200 OK\r\n"]),
-    gen_tcp:close(Sock)
-end.
-
-AcceptLoop = fun(LSock, Loop) ->
-    {ok, Sock} = gen_tcp:accept(LSock),
-    HandleRequest(Sock),
-    Loop(LSock, Loop)
-end.
-
-{ok, LSock} = gen_tcp:listen(8080,
-        [{packet, http}, {active, false}, {backlog, 1024}]).
-AcceptLoop(LSock, AcceptLoop).
+    gen_tcp:close(Sock).
