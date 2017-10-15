77c2c2285e592313c214ee9984412b68b8a7acc3
Sergey Yarkin <sega.yarkin@gmail.com>
2017-10-15 17:09:21 +0300

    stage5

--- web_server.erl
+++ web_server.erl
@@ -29,26 +29,48 @@ do_start(Port) ->
 
 accept_loop(LSock) ->
     {ok, Sock} = gen_tcp:accept(LSock),
-    handle_request(Sock),
+    handle_loop(Sock),
     accept_loop(LSock).
+
+
+handle_loop(Sock) ->
+    case handle_request(Sock) of
+        loop  -> handle_loop(Sock);
+        close -> gen_tcp:close(Sock)
+    end.
 
 
 handle_request(Sock) ->
     case parse_request(Sock) of
-        ignore -> ok;
+        ignore -> close;
+        {ok, #request{path    = Path,
+                      headers = #{'Connection' := Conn}}}
+                when Conn =:= "keep-alive";
+                     Conn =:= "Keep-Alive" ->
+            Body = ["The page '", Path, "' is cool, isn't it?"],
+            CLen = iolist_size(Body),
+            gen_tcp:send(Sock, [
+                "HTTP/1.0 200 OK\r\n",
+                "Connection: keep-alive\r\n",
+                ["Content-Length: ", integer_to_list(CLen), "\r\n"],
+                "\r\n",
+                Body
+            ]),
+            loop;
         {ok, #request{path = Path}} ->
             gen_tcp:send(Sock, [
                 "HTTP/1.0 200 OK\r\n",
                 "\r\n",
                 "The page '", Path, "' is cool, isn't it?"
-            ]);
+            ]),
+            close;
         {error, Error} ->
             io:format("Error: ~p", [Error]),
             gen_tcp:send(Sock, [
                 "HTTP/1.0 500 Internal Error\r\n"
-            ])
-    end,
-    gen_tcp:close(Sock).
+            ]),
+            close
+    end.
 
 
 parse_request(Sock) ->
